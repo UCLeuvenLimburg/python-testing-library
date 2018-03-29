a3269930aaae77213cb633740a4f05a98a204029
Frederic Vogels <frederic.vogels@gmail.com>
2018-03-29 14:12:51 +0200

    function_exists update
    function_exists now uses inspect to examine the caller's environment

--- ucll_scripting/testing/conditions.py
+++ ucll_scripting/testing/conditions.py
@@ -1,3 +1,4 @@
+import inspect
 from contextlib import contextmanager
 from ucll_scripting.testing.core import *
 
@@ -6,20 +7,36 @@ def never():
     return False
 
 
-def function_exists(function_name):
+def always():
+    return True
+
+
+def function_exists(identifier, module = None):
     '''
-    Checks if a function with the given name exists in the given module.
+    Checks if a variable with the given name exists.
+    If no module is specified, the variable is looked for
+    in the caller's environment (both locals and globals.)
     '''
-    parts = function_name.split('.')
-    current = globals()
-    
+    parts = identifier.split('.')
+
+    if module:
+        current = module.__dict__()
+    else:
+        caller_frame = inspect.currentframe().f_back
+        locals = caller_frame.f_locals
+        globals = caller_frame.f_globals
+        current = { **locals, **globals }
+
     for part in parts[:-1]:
         if part in current:
             current = dir(current[part])
         else:
             return never
-            
-    return lambda: function_name in current
+
+    if parts[-1] in current:
+        return always
+    else:
+        return never
 
 
 
