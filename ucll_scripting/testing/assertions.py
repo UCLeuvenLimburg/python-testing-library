47fa5d32424d2c32c385573bd29820ab84caef23
Frederic Vogels <frederic.vogels@gmail.com>
2018-04-10 21:04:56 +0200

    assertions

--- ucll_scripting/testing/assertions.py
+++ ucll_scripting/testing/assertions.py
@@ -1,30 +1,75 @@
-class __Equality:
-    def __init__(self, expected, actual):
-        self.__expected = expected
-        self.__actual = actual
+class Result:
+    pass
+
+class Success(Result):
+    def __bool__(self):
+        return True
+
+    def __and__(self, other):
+        return other
+
+    def __or__(self, other):
+        return self
+
+
+class Failure(Result):
+    def __init__(self, reason):
+        self.__reason = reason
+    
+    def __bool__(self):
+        return False
 
     @property
-    def success():
-        return self.__expected == self.__actual
+    def reason(self):
+        return self.__reason
 
-    @property
-    def message():
-        return f'Expected'
+    def __and__(self, other):
+        return self
 
-def equal(expected, actual):
-    return expected == actual
+    def __or__(self, other):
+        return other
 
-def approximately_equal(epsilon=0.0001):
-    return lambda expected, actual: abs(expected-actual) < epsilon
 
-def truthy(actual):
-    return True if actual else False
-
-def falsey(actual):
-    return False if actual else True
-
-def same_truthiness(expected, actual):
-    if is_truthy(expected):
-        return truthy(actual)
+def equal(*, expected, actual):
+    if expected == actual:
+        return Success()
     else:
-        return falsey(actual)
+        return Failure(f'Expected value: {expected}, actual value: {actual}')
+
+
+def equal_with_absolute_error(*, expected, actual, epsilon=0.0001):
+    if abs(expected-actual) < epsilon:
+        return Success()
+    else:
+        return Failure(f'Expected value: {expected} +- {epsilon}, actual value: {actual}')
+
+
+def equal_with_relative_error(*, expected, actual, percentage=0.01):
+    if (1 - percentage) * expected < actual < (1 + percentage) * expected:
+        return Success()
+    else:
+        return Failure(f'Expected value: {expected} +- {percentage}%, actual value: {actual}')
+
+
+def truthy(*, actual):
+    if actual:
+        return Success
+    else:
+        return Failure(f'Value {actual} is expected to be truthy')
+
+
+def falsey(*, actual):
+    if not actual:
+        return Success
+    else:
+        return Failure(f'Value {actual} is expected to be falsye')
+
+    
+def same_truthiness(*, expected, actual):
+    if expected:
+        return truthy(actual=actual)
+    else:
+        return falsey(actual=actual)
+
+
+
---
+++ ucll_scripting/tests/test_assertions.py
@@ -0,0 +1,56 @@
+from ucll_scripting.testing.assertions import *
+import unittest
+
+
+class AssertionTests(unittest.TestCase):
+    def test_equality(self):
+        assert equal(expected=4, actual=4)
+        assert equal(expected='a', actual='a')
+        assert equal(expected=[1, 2, 3], actual=[1, 2, 3])
+
+    def test_inequality(self):
+        assert not equal(expected=1, actual=3)
+        assert not equal(expected='a', actual='b')
+        assert not equal(expected=5, actual='b')
+        assert not equal(expected=[1, 2, 3], actual=[1, 2])
+        assert not equal(expected=[1, 2, 3], actual=[1, 2, 4])
+        assert not equal(expected=[1, 2, 3], actual=[3, 2, 1])
+
+    def test_truthy(self):
+        assert truthy(actual=True)
+        assert truthy(actual='x')
+        assert truthy(actual=1)
+        assert truthy(actual=[5])
+        assert truthy(actual={'a'})
+
+        assert not truthy(actual=False)
+        assert not truthy(actual=0)
+        assert not truthy(actual='')
+        assert not truthy(actual=[])
+        assert not truthy(actual={})
+
+    def test_falsey(self):
+        assert not falsey(actual=True)
+        assert not falsey(actual='x')
+        assert not falsey(actual=1)
+        assert not falsey(actual=[5])
+        assert not falsey(actual={'a'})
+
+        assert falsey(actual=False)
+        assert falsey(actual=0)
+        assert falsey(actual='')
+        assert falsey(actual=[])
+        assert falsey(actual={})
+        
+    def test_same_truthiness(self):
+        assert same_truthiness(expected=True, actual=True)
+        assert same_truthiness(expected=False, actual=False)
+        assert same_truthiness(expected=False, actual=0)
+        assert same_truthiness(expected='', actual={})
+
+        assert not same_truthiness(expected=True, actual=False)
+        assert not same_truthiness(expected=0, actual=5)
+        assert not same_truthiness(expected=True, actual={})
+        assert not same_truthiness(expected='bc', actual='')
+
+        
